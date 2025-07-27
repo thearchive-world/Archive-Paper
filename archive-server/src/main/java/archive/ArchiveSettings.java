53f8388057e0ce4e2d2a97cb9118be4cb4464b86
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-07-26 19:24:13 -0700

    update saving cli arg to have a bool value

--- archive-server/src/main/java/archive/ArchiveSettings.java
+++ archive-server/src/main/java/archive/ArchiveSettings.java
@@ -3,19 +3,20 @@ package archive;
 import net.minecraft.server.dedicated.DedicatedServer;
 
 public class ArchiveSettings {
-    private static final boolean disableSavingOption = DedicatedServer.getServer().options.has("archiveDisableSaving");
+    private static final boolean disableSavingOptionSet = DedicatedServer.getServer().options.has("archiveDisableSaving");
+    private static final boolean disableSavingOption = (boolean) net.minecraft.server.dedicated.DedicatedServer.getServer().options.valueOf("archiveDisableSaving");
     private static final boolean forceUpgradeOption = DedicatedServer.getServer().options.has("forceUpgrade");
 
     public static boolean disableSaving() {
-        // optional override to force disabling saving
-        if (disableSavingOption) {
-            return true;
+        // use set value if option is set
+        if (disableSavingOptionSet) {
+            return disableSavingOption;
         }
         // enable saving if forceUpgrade is set
         if (forceUpgradeOption) {
             return false;
         }
-        // default to disabling saving
-        return true;
+        // otherwise fallback to default
+        return disableSavingOption;
     }
 }
